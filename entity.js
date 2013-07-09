8f525e4b80ec519f365dfcb7946e92ab5ef1be2f
Richard <richard.habeeb@gmail.com>
2013-07-09 12:01:24 -0500

    Simplifying Entity

--- entity.js
+++ entity.js
@@ -1,19 +1,23 @@
 function Entity(layer) {
 	var self = this;
 	this.health = 100;
-	this.x = 100;
-	this.y = 100;
+	this.x = 0;
+	this.y = 0;
+	this.grid_x = 0;
+	this.grid_y = 0;
+	this.move_time = 0.15;
+	
 	
 	this.imageObj = new Image();
 	this.imageObj.src = 'images\\Brain Jelly.png';
 	
-	this.layer = layer; //this neeeds to know its layer for stupid reasons.
+	this.layer = layer; //this needs to know its layer for stupid reasons.
 	this.anim;
 	this.sprite = new Kinetic.Image({
 			x: this.x,
 			y: this.y,
-			width: 32,
-			height: 32,
+			width: PX_PER_CELL,
+			height: PX_PER_CELL,
 			image: this.imageObj,
 	});
 	
@@ -23,45 +27,39 @@ function Entity(layer) {
 	}
 	
 	
-	function SetupTween() {
-		self.anim = new Kinetic.Tween({
-			node: self.sprite,
-			x: self.x,
-			y: self.y,
-			duration: 0.15,
+	this.SetupTween = function () {
+		this.anim = new Kinetic.Tween({
+			node: this.sprite,
+			x: this.x,
+			y: this.y,
+			duration: this.move_time,
 			easing: Kinetic.Easings.Linear
 		});
-		self.anim.play();
+		this.anim.play();
 	}
 	
 	this.MoveUp = function() {
-		if(this.anim == null || (typeof this.anim === "object" && this.anim.tween._time >= this.anim.tween.duration)) {
-			this.y = Math.max(0,this.y-32);
-			SetupTween();
-		}
+		this.Move(function() { self.y = Math.max(0, self.y-PX_PER_CELL); });
 	}
 	
 	this.MoveDown = function() {
-		if(this.anim == null || (typeof this.anim === "object" && this.anim.tween._time >= this.anim.tween.duration)) {
-			this.y = Math.min(640,this.y+32);
-			SetupTween();
-		}
+		this.Move(function() { self.y = Math.min(WINDOW_HEIGHT_PX, self.y+PX_PER_CELL); });
 	}
 	
 	this.MoveLeft = function() {
-		if(this.anim == null || (typeof this.anim === "object" && this.anim.tween._time >= this.anim.tween.duration)) {
-			this.x = Math.max(0,this.x-32);
-			SetupTween();
-		}
+		this.Move(function() { self.x = Math.max(0, self.x-PX_PER_CELL); });
 	}
 	
 	this.MoveRight = function() {
+		this.Move(function() { self.x = Math.min(WINDOW_WIDTH_PX, self.x+PX_PER_CELL); });
+	}
+	
+	this.Move = function(adjustXY) {
 		if(this.anim == null || (typeof this.anim === "object" && this.anim.tween._time >= this.anim.tween.duration)) {
-			this.x = Math.min(640,this.x+32);
-			SetupTween();
+			adjustXY();
+			this.SetupTween();
 		}
 	}
-
 	
 	
 
